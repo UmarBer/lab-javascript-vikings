89195968105fb76565fcc5da318a23cf87dce76f
Umar Bermejo <umarber94@gmail.com>
2022-02-08 21:50:49 +0100

    9 tests passed so far

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,31 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+}
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
