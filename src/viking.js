ac2fd41d40da4c8df2c42c877fa785f535793b01
Umar Bermejo <umarber94@gmail.com>
2022-02-09 21:03:56 +0100

    Iterations 1 through 4 complete

--- src/viking.js
+++ src/viking.js
@@ -44,7 +44,43 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let damage = this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+
+    if (this.saxonArmy[0].health <= 0) {
+      this.saxonArmy.pop(this.saxonArmy[0]);
+    }
+    return damage;
+  }
+  saxonAttack() {
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let damage = this.vikingArmy[0].receiveDamage(this.saxonArmy[0].strength);
+
+    if (this.vikingArmy[0].health <= 0) {
+      this.vikingArmy.pop(this.vikingArmy[0]);
+    }
+    return damage;
+  }
+  showStatus() {}
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
